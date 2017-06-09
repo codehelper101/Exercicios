8e50798b392475535bb425cd5562c2f1995edb3e
codehelper101 <jorgeeduardoaoliveira@gmail.com>
2017-06-09 14:19:55 -0300

    Add files via upload

---
+++ Exercicio2/exercicio2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Divisor{
+public static void main (String [] args){
+Scanner scanner=new Scanner(System.in);
+System.out.println("digite um numero:");
+Int num=scanner.nextInt();
+for(int j=1;j<=num;j++){
+If(num%j==0){
+System.ouy.println(j);
+}
+}
+}
+}
