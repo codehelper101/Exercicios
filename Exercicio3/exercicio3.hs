6da698b6e9138ec348793e528cedd24c1e000bfe
Samuel Henrique <samosaara@gmail.com>
2017-06-09 19:11:49 -0300

    Create exercicio3.hs

---
+++ Exercicio3/exercicio3.hs
@@ -0,0 +1,11 @@
+isqrt :: (Integral a) => a -> a
+prime :: (Integral a) => a -> Bool
+
+isqrt a = ceiling (sqrt (fromIntegral a))
+
+prime a = null [x | x <- (2 : [3, 5..isqrt a]), a `mod` x == 0]
+
+main = do  
+    putStrLn "digite um numero e saiba se ele é par"  
+    name <- getLine  
+    print (prime (read name :: Int))
