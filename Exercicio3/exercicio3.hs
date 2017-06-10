2615638e1c40de9ae0e1080902961d84e70bd543
Samuel Henrique <samosaara@gmail.com>
2017-06-10 10:29:24 -0300

    Create exercicio3.hs
    corrige palavra errada

--- Exercicio3/exercicio3.hs
+++ Exercicio3/exercicio3.hs
@@ -6,6 +6,6 @@ isqrt a = ceiling (sqrt (fromIntegral a))
 prime a = null [x | x <- (2 : [3, 5..isqrt a]), a `mod` x == 0]
 
 main = do  
-    putStrLn "digite um numero e saiba se ele é par"  
+    putStrLn "digite um numero e saiba se ele é primo"  
     name <- getLine  
     print (prime (read name :: Int))
