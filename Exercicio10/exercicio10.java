60c72ca540d2d785fc6cac545319e73b1565c535
codehelper101 <jorgeeduardoaoliveira@gmail.com>
2017-06-28 15:27:47 -0300

    Add files via upload

---
+++ Exercicio10/exercicio10.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class exercicio10 {
+
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+       System.out.println("informe numero:");
+       int num=scanner.nextInt();
+       for (int i= num; i >= 0; i--) {
+           System.out.print(i+" ");
+       
+    }
+
+}
+}
