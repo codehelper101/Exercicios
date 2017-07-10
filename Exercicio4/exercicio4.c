6cca1fcb2987d21687807fe470be17eee5a3f069
Antony Araújo <contato.antonyaraujo@gmail.com>
2017-07-09 22:10:13 -0300

    Add files via upload

---
+++ Exercicio4/exercicio4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+     // Variáveis
+     int value;
+
+     // Desenvolvimento
+     printf ("Informe o valor: \n");
+     scanf ("%i", &value);
+
+     if (value%2 == 0){
+        printf ("É um valor par \n");
+     }
+       else
+         printf("É um valor ímpar \n");
+
+
+
+
+  return 0;
+}
